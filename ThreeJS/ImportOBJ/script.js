1c172528c9ca1587581a6fae32be1047541e2e00
Vladimir Minkin <vladimir.minkin@gmail.com>
2014-04-10 13:25:58 +0400

    Final Commit from page

--- ThreeJS/ImportOBJ/script.js
+++ ThreeJS/ImportOBJ/script.js
@@ -1,163 +1,173 @@
 (function () {
 	
-	// uglifyjs OBJLoader.js -o OBJLoader.min.js -p 5 -c -m
-	// uglifyjs MTLLoader.js -o MTLLoader.min.js -p 5 -c -m
-	// uglifyjs OBJMTLLoader.js -o OBJMTLLoader.min.js -p 5 -c -m
-	// uglifyjs WebGLRenderer3.js -o WebGLRenderer3.min.js -p 5 -c -m
-	// uglifyjs TrackballControls.js -o TrackballControls.min.js -p 5 -c -m
-	// uglifyjs SoftwareRenderer.js -o SoftwareRenderer.min.js -p 5 -c -m
-
-	// python convert_obj_three.py -i dragon_lowpoly.obj -o dragon_lowpoly.js
-	// convert_to_threejs.py dragon_lowpoly_fbx.fbx dragon_lowpoly_fbxjs -g -l
 	var SCALE = 50;
 
-	var stats;
-	var mouseX = 0, mouseY = 0;
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
+	var _stats;
+	var _camera, _controls, _lightDirectional, _lightAmbient, _scene, _renderer;
+    var _manager, _loader, _geometry, _materialMesh, _materialVertex, _mesh, _vertexColor;
 
-	var camera, directionalLight, ambient, scene, renderer;
-    var manager, loader, geometry, material, mesh;
+    // GRID SETTINGS
+    var _gridFloor 			= 0, 
+    	_gridStep 			= 100, 
+    	_gridResolution 	= 12, 
+    	_gridOffset 		= _gridResolution * _gridStep * 0.5;
 
-    init();
-	animate();
+    // RANDOM SHPERE SETTINGS	
+    var _randomShpereCounter 		= 10,
+		_randomShpereSize 			= 10,
+		_randomShpereDivisions 		= 8,
+		_randomShpereMaterialParams = {
+			ambient 	: 0xffcc00,
+			color 		: 0xFFcc00, 
+			specular 	: 0x555555, 
+			shininess 	: 30 
+		};
+
+    Init();
+	Animate();
 
 	window.addEventListener( 'resize', Handle_WindowResize, false );
 
-    function init() {
+    function Init() {
+       	
+       	/* INITIALIZE */
+       	Initialize();
+		/* CREATE GRID */
+		Create_Grid();
+		/* CREATE RANDOM SPHERES */
+		Create_RandomShperes();
+		/* LOAD AND CREATE OBJECTS FROM FILE */
+		Create_LoadObjects();
 
-        camera 		= new THREE.PerspectiveCamera( 40, windowHalfX / windowHalfY, 1, 10000 );
-        scene 		= new THREE.Scene();
-       	controls 	= new THREE.TrackballControls( camera );
-        
-        // material 	= new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
-        // material 	= new THREE.MeshBasicMaterial( { color: 0xff00ff, vertexColors: THREE.FaceColors } );
-       	material 	= new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true, vertexColors: THREE.FaceColors } );
-       	// material2 	= new THREE.MeshPhongMaterial( { ambient: 0x050505, color: 0x000000, specular: 0x555555, shininess: 30 } );
-       	// material2 	= new THREE.LineBasicMaterial( { color: 0x303030 } ),
-       	// material2 	= new THREE.MeshBasicMaterial({color: 0x00ff00 });
-       	material2 	= new THREE.MeshBasicMaterial( { color: 0x00ff00, vertexColors: THREE.FaceColors } );
-       	staticolor	= 0x212121;//Math.random() * 0xFFFFFF;
-
-        manager 	= new THREE.LoadingManager();
-        // loader 		= new THREE.OBJMTLLoader( manager );
-        loader 		= new THREE.JSONLoader( manager );
-
-        directionalLight = new THREE.DirectionalLight( 0xffffff );
-        ambient 	= new THREE.AmbientLight( 0xFFFFFF );
-		
-		// Grid
-		var grig_material 	= new THREE.LineBasicMaterial( { color: 0x303030 } ),
-			grig_geomentry  = new THREE.Geometry(),
-			floor = 0, step = 100, grids = 12, grid_offset = grids*step*0.5;
-
-		for ( var i = 0; i <= grids; i ++ ) {
-			grig_geomentry.vertices.push( new THREE.Vector3( - grid_offset, floor, i * step - grid_offset ) );
-			grig_geomentry.vertices.push( new THREE.Vector3(   grid_offset, floor, i * step - grid_offset ) );
-
-			grig_geomentry.vertices.push( new THREE.Vector3( i * step - grid_offset, floor, -grid_offset ) );
-			grig_geomentry.vertices.push( new THREE.Vector3( i * step - grid_offset, floor,  grid_offset ) );
-		}
-
-		var line = new THREE.Line( grig_geomentry, grig_material, THREE.LinePieces );
-		scene.add( line );
-
-		// loader.load( 'dragon_lowpoly_withmat.obj', 'dragon_lowpoly_withmat.mtl', Handle_LoaderComplete);
-		
-		var phys_sphere_counter = 10;
-		var counter = phys_sphere_counter;
-		var sphere_size = 10;
-		var sphere_div = 8;
-		var sphere_geom, vertex, sphere_mesh;
-		var matparam = { ambient: 0x050505, color: 0x000000, specular: 0x555555, shininess: 30 };
-		var cubelimits = grid_offset * 2;
-		while(counter--) {
-			sphere_geom = new THREE.SphereGeometry( sphere_size, sphere_div, sphere_div );
-			sphere_mesh = new THREE.Mesh( sphere_geom, new THREE.MeshPhongMaterial( matparam ) );
-			sphere_mesh.position.set( ( Math.random() - 0.5 ) * cubelimits, ( Math.random() - 0.5 ) * cubelimits, ( Math.random() - 0.5 ) * cubelimits );
-			console.log(sphere_mesh.position);
-			scene.add( sphere_mesh );
-		}
-
-
-		loader.load("dragon_lowpoly.js", function (geometry) {
-			var i, j, k;
-			for ( i = 0, j = geometry.faces.length; i < j; i ++ ) {
-				// geometry.faces[ i ].color.setHex( Math.random() * 0xffffff );
-				geometry.faces[ i ].color.setRGB( Math.random(), Math.random(), Math.random() );
-			}
-
-			for ( k = geometry.vertices.length - 1; k >= 0; k-- ) {
-				vertex = geometry.vertices[k];
-				sphere_geom = new THREE.SphereGeometry( 0.075, 6, 6 );
-				for ( i = 0, j = sphere_geom.faces.length; i < j; i ++ ) {
-					sphere_geom.faces[ i ].color.setHex( staticolor );
-				}
-				sphere_mesh = new THREE.Mesh(sphere_geom, material2);
-				sphere_mesh.position.x = vertex.x;
-				sphere_mesh.position.y = vertex.y;
-				sphere_mesh.position.z = vertex.z;
-				// sphere.scale.x = sphere.scale.y = sphere.scale.z = SCALE;
-				THREE.GeometryUtils.merge(geometry, sphere_mesh);
-			};
-			mesh = new THREE.Mesh(geometry, material);
-			mesh.scale.x = mesh.scale.y = mesh.scale.z = SCALE;
-			scene.add( mesh );
-		})
-
-		// 
-		// directionalLight.position.set( 1, 1, 0 ).normalize();
-		camera.position.z = 1500;
-		camera.position.y = 500;
-		camera.position.x = 1500;
-
-		 // scene.add( directionalLight );
-		 // scene.add( ambient );
-
-        // renderer = new THREE.CanvasRenderer();
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-		renderer.setClearColor( 0xffffff, 1 );
-		// renderer = new THREE.SoftwareRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        
-        stats = new Stats();
-		stats.domElement.style.position = 'absolute';
-		stats.domElement.style.top = '0px';
-		stats.domElement.style.zIndex = 100;
-
-		document.body.appendChild( stats.domElement );
-		document.body.appendChild( renderer.domElement );
+		Setup_Renderer();
+		Setup_Camera();
+		Setup_Lights();
+		Setup_Stats();
 	}
 
-    function animate() {
-        requestAnimationFrame( animate );
-       	renderer.render( scene, camera );
-        stats.update();
-        controls.update();
-    }
-
-    function Handle_LoaderComplete (object) {
-    	mesh = object; //new THREE.Mesh(object);
-    	geometry = mesh.geometry
-    	console.log(mesh, geometry);
-		// mesh.traverse( function ( child ) {
-		// 	if ( child instanceof THREE.Object3D  ) {
-		// 		// geometry = child.geometry
-		// 		// for ( var i = 0, j = geometry.faces.length; i < j; i ++ ) geometry.faces[ i ].color.setHex( Math.random() * 0xffffff );
-		// 		// child.material = material;
-		// 		 child.material.color.setRGB (1, 1, 0);
-		// 		return;
-		// 	}
-		// } );
-		
-		scene.add( mesh );
+    function Animate() {
+        requestAnimationFrame( Animate );
+       	_renderer.render( _scene, _camera );
+        _stats.update();
+        _controls.update();
     }
 
 	function Handle_WindowResize (event) {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
+		_camera.aspect = window.innerWidth / window.innerHeight;
+		_camera.updateProjectionMatrix();
 
-		renderer.setSize( window.innerWidth, window.innerHeight );
+		_renderer.setSize( window.innerWidth, window.innerHeight );
+	}
+
+	function Setup_Stats () {
+		_stats = new Stats();
+		_stats.domElement.style.position = 'absolute';
+		_stats.domElement.style.top = '0px';
+		_stats.domElement.style.zIndex = 100;
+		document.body.appendChild( _stats.domElement );
+	}
+
+	function Setup_Lights () {
+		_lightDirectional.position.set( 1, 1, 0 ).normalize();
+		_scene.add( _lightDirectional );
+		_scene.add( _lightAmbient );
+	}
+
+	function Setup_Camera () {
+		_camera.position.x = 1500;
+		_camera.position.y = 500;
+		_camera.position.z = 1500;
+	}
+
+	function Setup_Renderer () {
+		/* SOME OTHER RENDRERS */
+        // _renderer = new THREE.CanvasRenderer();
+		// _renderer = new THREE.SoftwareRenderer();
+        _renderer = new THREE.WebGLRenderer( { antialias: true } );
+		_renderer.setClearColor( 0x2222222, 1 );
+        _renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( _renderer.domElement );
+	}
+
+	function Create_LoadObjects () {
+		_loader.load("dragon_lowpoly_fromeditor.js", function (_geometry) {
+			var i, j, k;
+			for ( i = 0, j = _geometry.faces.length; i < j; i ++ ) {
+				_geometry.faces[ i ].color.setHex( Math.random() * 0xffffff ); // same _geometry.faces[ i ].color.setRGB( Math.random(), Math.random(), Math.random() );
+			}
+
+			var sphere_geom, vertex, sphere_mesh;
+			for ( k = _geometry.vertices.length - 1; k >= 0; k-- ) {
+				vertex = _geometry.vertices[k];
+				sphere_geom = new THREE.SphereGeometry( 0.035, 6, 6 );
+				for ( i = 0, j = sphere_geom.faces.length; i < j; i ++ ) { 
+					sphere_geom.faces[ i ].color.setHex( _vertexColor ); 
+				}
+				sphere_mesh = new THREE.Mesh(sphere_geom, _materialVertex);
+				sphere_mesh.position.x = vertex.x;
+				sphere_mesh.position.y = vertex.y;
+				sphere_mesh.position.z = vertex.z;
+				THREE.GeometryUtils.merge(_geometry, sphere_mesh);
+			};
+			_mesh = new THREE.Mesh(_geometry, _materialMesh);
+			_mesh.scale.x = _mesh.scale.y = _mesh.scale.z = SCALE;
+			_scene.add( _mesh );
+		})
+	}
+
+	function Create_RandomShperes () {
+		var counter = _randomShpereCounter;
+		var sphere_geom, vertex, sphere_mesh;
+		var cubelimits = _gridOffset * 2;
+		while(counter--) {
+			sphere_geom = new THREE.SphereGeometry( _randomShpereSize, _randomShpereDivisions, _randomShpereDivisions );
+			sphere_mesh = new THREE.Mesh( sphere_geom, new THREE.MeshPhongMaterial( _randomShpereMaterialParams ) );
+			sphere_mesh.position.set( ( Math.random() - 0.5 ) * cubelimits, ( Math.random() - 0.5 ) * cubelimits, ( Math.random() - 0.5 ) * cubelimits );
+			_scene.add( sphere_mesh );
+		}
+	}
+
+	function Create_Grid () {
+		var grig_material 	= new THREE.LineBasicMaterial( { color: 0x303030 } ),
+			grig_geomentry  = new THREE.Geometry(),
+			_gridFloor = 0, _gridStep = 100, _gridResolution = 12, _gridOffset = _gridResolution*_gridStep*0.5;
+
+		for ( var i = 0; i <= _gridResolution; i ++ ) {
+			grig_geomentry.vertices.push( new THREE.Vector3( - _gridOffset, _gridFloor, i * _gridStep - _gridOffset ) );
+			grig_geomentry.vertices.push( new THREE.Vector3(   _gridOffset, _gridFloor, i * _gridStep - _gridOffset ) );
+
+			grig_geomentry.vertices.push( new THREE.Vector3( i * _gridStep - _gridOffset, _gridFloor, -_gridOffset ) );
+			grig_geomentry.vertices.push( new THREE.Vector3( i * _gridStep - _gridOffset, _gridFloor,  _gridOffset ) );
+		}
+
+		var line = new THREE.Line( grig_geomentry, grig_material, THREE.LinePieces );
+		_scene.add( line );
+	}
+
+	function Initialize() {
+		var windowHalfX = window.innerWidth / 2;
+		var windowHalfY = window.innerHeight / 2;
+
+        _camera 			= new THREE.PerspectiveCamera( 40, windowHalfX / windowHalfY, 1, 10000 );
+        _scene 				= new THREE.Scene();
+       	_controls 			= new THREE.TrackballControls( _camera );
+
+		_materialMesh 		= new THREE.MeshBasicMaterial( { color: 0xffFFff, wireframe: true, vertexColors: THREE.FaceColors } );
+       	_materialVertex 	= new THREE.MeshBasicMaterial( { color: 0x00ff00, vertexColors: THREE.FaceColors } );
+       	_vertexColor			= 0xeeeeee;
+
+        _manager 			= new THREE.LoadingManager();
+        _loader 			= new THREE.JSONLoader( _manager );
+
+        _lightDirectional 	= new THREE.DirectionalLight( 0xffffff );
+        _lightAmbient 		= new THREE.AmbientLight( 0xFFFFFF );
+
+        /* SOME OTHER TEST MATERIALS */
+        // _materialMesh 	= new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
+        // _materialMesh 	= new THREE.MeshBasicMaterial( { color: 0xff00ff, vertexColors: THREE.FaceColors } );
+       	// _materialVertex 	= new THREE.MeshPhongMaterial( { _lightAmbient: 0x050505, color: 0x000000, specular: 0x555555, shininess: 30 } );
+       	// _materialVertex 	= new THREE.LineBasicMaterial( { color: 0x303030 } ),
+       	// _materialVertex 	= new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	}
 
 })();
